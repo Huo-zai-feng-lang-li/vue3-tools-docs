e952664fa0dc7b8db65f50b2142f0893d10a1dd8
ZK <1334132303@qq.com>
2024-09-09 22:30:17 +0800

    chore(icon): 更换icon

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   markdown: {
     lineNumbers: true
   },
-  head: [["link", { rel: "icon", href: "/favicon.ico" }]],
+  head: [["link", { rel: "icon", href: "/favicon.png" }]],
   themeConfig: {
     search: {
       provider: "local"
